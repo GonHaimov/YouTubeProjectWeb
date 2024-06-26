b3a622b85a5bd4b52bb8131b33a731ba62205f8a
GonHaimov <gonmatan12@gmail.com>
2024-06-26 16:55:05 +0300

    Add AP_Ex_1_web as a regular directory

---
+++ YouTubeProject/AP_Ex_1_web/src/components/videoLogic/VideoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos, onVideoSelect, onEdit, onDelete, loggedInUser }) => {
+  const renderedVideos = videos.map((video) => {
+    return <VideoItem
+      key={video.id}
+      video={video}
+      onVideoSelect={() => onVideoSelect(video)}
+      onEdit={onEdit}
+      onDelete={onDelete}
+      loggedInUser={loggedInUser}
+    />;
+  });
+
+  return <div className="row">{renderedVideos}</div>;
+};
+
+export default VideoList;
