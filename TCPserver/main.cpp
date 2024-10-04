3604a6f77d8c21caf67e6e387ea688040e20a6be
GonHaimov <gonmatan12@gmail.com>
2024-10-04 18:13:52 +0300

    tcp server

--- TCPserver/main.cpp
+++ TCPserver/main.cpp
@@ -1,27 +1,43 @@
 #include <sstream>
-#include <map>
 #include <iostream>
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <thread>  // <--- Make sure to include this for std::thread
+#include <thread>
+#include "RecommendationSystem.hpp"  // Include the header
 
-void handleClient(int client_sock) {
+void handleClient(int client_sock, RecommendationSystem& recSystem) {
     char buffer[4096];
     ssize_t bytes_received;
 
-    // Receive data from the client
     while ((bytes_received = recv(client_sock, buffer, sizeof(buffer), 0)) > 0) {
-        // Echo the received data back to the client
-        send(client_sock, buffer, bytes_received, 0);
+        buffer[bytes_received] = '\0'; // Null-terminate the received data
+        std::istringstream iss(buffer);
+        std::string command, userId, videoId;
+
+        iss >> command >> userId >> videoId;
+        std:: cout << command << userId << videoId << std:: endl;
+        if (command == "watch") {
+            recSystem.updateWatchHistory(userId, videoId);
+        } else if (command == "recommend") {
+            recSystem.updateWatchHistory(userId, videoId);
+            std::vector<std::string> recommendations = recSystem.getRecommendations(userId);
+            std::ostringstream oss;
+            for (const std::string& rec : recommendations) {
+                oss << rec << " ";
+            }
+            std::string response = oss.str();
+            std:: cout << response.c_str() << std:: endl;
+            send(client_sock, response.c_str(), response.size(), 0);
+        }
     }
 
     if (bytes_received < 0) {
         perror("error receiving data");
     }
 
-    close(client_sock); // Close the client socket
+    close(client_sock);
 }
 
 int main() {
@@ -49,18 +65,20 @@ int main() {
         return 1;
     }
 
+    RecommendationSystem recSystem;  // Create the RecommendationSystem object
+
     while (true) {
         struct sockaddr_in client_sin;
         unsigned int addr_len = sizeof(client_sin);
         int client_sock = accept(sock, (struct sockaddr*)&client_sin, &addr_len);
 
         if (client_sock < 0) {
-            perror("error accepting client");  // Fixed typo here
+            perror("error accepting client");
             continue;
         }
 
         // Create a new thread to handle communication with the client
-        std::thread client_thread(handleClient, client_sock);
+        std::thread client_thread(handleClient, client_sock, std::ref(recSystem));
         client_thread.detach(); // Detach the thread to run independently
     }
 
